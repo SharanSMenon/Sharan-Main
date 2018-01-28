fadff41096f1d454456084d92603fe092e279d36
Sharan Sajiv Menon <sharansajivmenon@gmail.com>
2018-01-28 16:39:15 -0500

    Created basic express app

---
+++ nodeJs/webserver/app.js
@@ -0,0 +1,38 @@
+// import { read } from 'fs';
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('currentYear',() => {
+    return new Date().getFullYear();
+});
+app.get('/',(req, res) => {
+    res.render('home.hbs', {
+        pageTitle:"Home",
+        message:"Welcome"
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About',
+    });
+})
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+});
